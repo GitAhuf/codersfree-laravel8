6ff761deb4c0c788ba9cdb01e4d5a23254c482c1
ahuf <recibosalvaro@gmail.com>
2023-06-05 22:33:15 -0500

    Registrar usuario desde el cliente

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 
+use Illuminate\Support\Facades\Http;
+
 class RegisteredUserController extends Controller
 {
     /**
@@ -39,10 +41,41 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        $response = Http::withHeaders([
+            'Acept' => 'aplication/json'
+        ])->post('http://api.codersfree.test/v1/register', $request->all());
+
+        if($response->status() == 422){
+            return back()->withErrors($response->json()['errors']);
+        }
+
+        $service = $response->json();
+
         $user = User::create([
             'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'email' => $request->email           
+        ]);
+
+        // PETICION HTTP PARA ACCESS TOKEN
+        $response = Http::withHeaders([
+            'Accept' => 'aplication/json'
+        ])->post('http://api.codersfree.test/oauth/token', [
+            'grant_type' => 'password',
+            'client_id' => '9957a452-c219-4167-b02a-036d97716cc8',
+            'client_secret' => 'ues1nC0PFXVheXv7hitiVb3lU1J2XjGZjw9wJ8JI',
+            'username' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        // ALMACENA LA INFORMACION
+        $access_token = $response->json(); 
+
+        // CREA UN NUEVO REGISTRO EN LA TABLA ACCESS_TOKEN RELACIONANDOLO CON UN DETERMINADO USUARIO
+        $user->accessToken()->create([
+            'service_id' => $service['data']['id'],
+            'access_token' => $access_token['access_token'],
+            'refresh_token' => $access_token['refresh_token'],
+            'expires_at' => now()->addSecond($access_token['expires_in'])
         ]);
 
         event(new Registered($user));
